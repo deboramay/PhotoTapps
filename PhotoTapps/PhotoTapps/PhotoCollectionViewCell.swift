8f3b1b2c9e0d1b244aa69743daf5bec324d68df5
Anastasia Shkalikova <anastasia.shkalikova@gmail.com>
2022-02-14 20:24:28 +0200

    Final Version

---
+++ PhotoTapps/PhotoTapps/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoTapps
+//
+//  Created by Anastasia on 13.02.2022.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var dogImageView: UIImageView!
+}
---
+++ PhotoTapps/PhotoTapps/PhotoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoViewController.swift
+//  PhotoTapps
+//
+//  Created by Anastasia on 13.02.2022.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+  
+  var image: UIImage?
+
+  @IBOutlet weak var photoImageView: UIImageView!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+    photoImageView.image = image
+    }
+    
+  @IBAction func shareAction(_ sender: UIButton) {
+    
+    let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+    
+    shareController.completionWithItemsHandler = { _, bool, _, _ in
+      if bool {
+        print("OK!")
+      }
+    }
+    
+    present(shareController, animated: true, completion: nil)
+  }
+}
